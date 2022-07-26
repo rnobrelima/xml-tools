218b8d7e4962f9b08dfd5021fe45ca950f0fa934
Rodrigo Nobre <rnobrelima@gmail.com>
2022-07-26 16:08:02 -0300

    init project

---
+++ xml-tools/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+
+namespace xml_tools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FindDuplicatesGuidsTiss();
+        }
+
+        static void FindDuplicatesGuidsTiss()
+        {
+            var filePath = "D:\\XML";
+            var archives = Directory.GetFiles(filePath);
+
+            foreach (string fileName in archives)
+            {
+
+                XmlDocument xml = new XmlDocument();
+
+                xml.Load(fileName);
+                List<string> guides = new List<string>();
+                List<string> guidesDuplicates = new List<string>();
+
+                var nodes = xml.GetElementsByTagName("ans:numeroGuiaPrestador");
+
+                for (int i = 0; i < nodes.Count; i++)
+                {
+                    if (guides.Contains(nodes.Item(i).InnerText))
+                        guidesDuplicates.Add(nodes.Item(i).InnerText);
+
+                    guides.Add(nodes.Item(i).InnerText);
+
+
+                }
+                if (guidesDuplicates.Count > 0)
+                {
+                    Console.WriteLine(String.Format("File: " + fileName));
+                    Console.WriteLine(String.Format("Guides Duplicates: " + String.Join(",", guidesDuplicates)));
+
+                }
+                else
+                {
+                    Console.WriteLine(String.Format("Files: " + fileName + " No Guides Duplicates!"));
+                }
+            }
+        }
+    }
+}
+
